8343157f00907a7a783c8b24d3d58623432621b3
limingzxc <2904962732@qq.com>
2023-10-19 15:24:06 +0800

    replace inject

--- src/main/java/net/fabricmc/clienttools/mixin/MinecraftMixin.java
+++ src/main/java/net/fabricmc/clienttools/mixin/MinecraftMixin.java
@@ -3,11 +3,11 @@ package net.fabricmc.clienttools.mixin;
 import net.fabricmc.clienttools.AutoClick;
 import net.minecraft.client.Minecraft;
 import net.minecraft.src.*;
-import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -84,8 +84,12 @@ public abstract class MinecraftMixin {
     private AutoClick playerControllerTool;
 
 
-    @Inject(method = "runTick", at = @At("HEAD"), cancellable = true)
-    private void runTick(CallbackInfo info) {
+    /**
+     * @author limingzxc
+     * @reason Expand auto-pounding block functionality
+     */
+    @Overwrite
+    public void runTick() {
         if (this.rightClickDelayTimer > 0) {
             --this.rightClickDelayTimer;
         }
@@ -495,7 +499,6 @@ public abstract class MinecraftMixin {
         this.mcProfiler.endSection();
         this.systemTime = Minecraft.getSystemTime();
 
-        info.cancel();
     }
 
 
